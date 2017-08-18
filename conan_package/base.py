1b224cccebe9a6edf62bd42921cd964d6ab6dbf7
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2017-08-18 11:38:48 +0300

    bump requirements versions

--- conan_package/base.py
+++ conan_package/base.py
@@ -12,7 +12,7 @@ class PaymentChannelBase(ConanFile):
     build_policy = "missing"
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    requires = "Common/0.1@joystream/stable"
+    requires = "Common/0.1.1@joystream/stable"
 
     def source(self):
         raise Exception("abstract base package was exported")
